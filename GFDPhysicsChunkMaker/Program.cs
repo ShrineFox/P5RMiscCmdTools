7391648cb033a536ed86dd8f73da46a95878ad54
ShrineFox <ShrineFox@users.noreply.github.com>
2026-02-15 16:02:53 -0500

    Add physics chunk & Credits bin programs

---
+++ GFDPhysicsChunkMaker/Program.cs
@@ -0,0 +1,88 @@
+﻿using System.Formats.Tar;
+
+namespace GFDPhysicsChunkMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var ymlLines = File.ReadAllLines(args[0]);
+            List<BoneEntry> boneEntries = new List<BoneEntry>();
+            List<PairEntry> pairEntries = new List<PairEntry>();
+            string list2 = "";
+
+            for (int i = 0; i < ymlLines.Length; i++)
+            {
+                if (!ymlLines[i].StartsWith("-") && !ymlLines[i].StartsWith("E"))
+                    continue;
+
+                if (ymlLines[i].StartsWith("- RestoringForce: "))
+                {
+                    BoneEntry bone = new BoneEntry();
+                    bone.RestoringForce = Convert.ToSingle(ymlLines[i].Replace("- RestoringForce: ", ""));
+                    bone.WindRate = Convert.ToSingle(ymlLines[i + 1].Replace("  WindRate: ", ""));
+                    bone.Mass = Convert.ToSingle(ymlLines[i + 2].Replace("  Mass: ", ""));
+                    bone.SphereRadius = Convert.ToSingle(ymlLines[i + 3].Replace("  SphereRadius: ", ""));
+                    bone.NodeName = ymlLines[i + 4].Replace("  NodeName: ", "");
+                    boneEntries.Add(bone);
+                    continue;
+                }
+
+                /*
+                if (ymlLines[i].StartsWith("Entry2List"))
+                {
+                    int x = i;
+                    while (!ymlLines[x].Contains("Entry3List"))
+                    {
+                        list2 += ymlLines[x] + "\r\n";
+                    }
+                    continue;
+                }*/
+
+                if (ymlLines[i].StartsWith("- LengthSq: "))
+                {
+                    PairEntry pair = new PairEntry();
+                    pair.LengthSq = Convert.ToSingle(ymlLines[i].Replace("- LengthSq: ", ""));
+                    pair.AngularLimit = Convert.ToSingle(ymlLines[i + 1].Replace("  AngularLimit: ", ""));
+                    pair.ChainThickness = Convert.ToSingle(ymlLines[i + 2].Replace("  ChainThickness: ", ""));
+                    pair.ParentBoneIndex = Convert.ToInt32(ymlLines[i + 3].Replace("  ParentBoneIndex: ", ""));
+                    pair.ChildBoneIndex = Convert.ToInt32(ymlLines[i + 4].Replace("  ChildBoneIndex: ", ""));
+                    pairEntries.Add(pair);
+                    continue;
+                }
+            }
+
+            string outputTsv = "Bone Index\tBone Name\t0 SphereRadius\t4 Mass\t8 RestoringForce\tC WindRate\r\n";
+            int b = 0;
+            foreach(var bone in boneEntries)
+            {
+                outputTsv += $"{b}\t{bone.NodeName}\t{bone.SphereRadius}\t{bone.Mass}\t{bone.RestoringForce}\t{bone.WindRate}\r\n";
+                b++;
+            }
+            outputTsv += "Link Parent\tLink Child\t\"Mass\" LengthSq \tUnk4 AngularLimit\t\"Radius\" ChainThickness \r\n";
+            foreach (var pair in pairEntries)
+            {
+                outputTsv += $"{pair.ParentBoneIndex}\t{pair.ChildBoneIndex}\t{pair.LengthSq}\t{pair.AngularLimit}\t{pair.ChainThickness}\r\n";
+            }
+            File.WriteAllText("output.tsv", outputTsv);
+        }
+    }
+
+    public class PairEntry
+    {
+        public float LengthSq { get; set; } = 0f;
+        public float AngularLimit { get; set; } = 0f;
+        public float ChainThickness { get; set; } = 0f;
+        public int ParentBoneIndex { get; set; } = 0;
+        public int ChildBoneIndex { get; set; } = 0;
+    }
+
+    public class BoneEntry
+    {
+        public float SphereRadius { get; set; } = 0f;
+        public float Mass { get; set; } = 0f;
+        public float RestoringForce { get; set; } = 0f;
+        public float WindRate { get; set; } = 0f;
+        public string NodeName { get; set; } = "";
+    }
+}
---
+++ P5RCreditsConverter/Program.cs
@@ -0,0 +1,110 @@
+﻿namespace P5RCreditsConverter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args[0].EndsWith(".bin"))
+            {
+                string creditsBinPath = args[0];
+                string outputTxtPath = ".\\out.txt";
+
+                using (FileStream fs = new FileStream(creditsBinPath, FileMode.Open))
+                using (BinaryReader br = new BinaryReader(fs))
+                {
+                    CreditsBin creditsBin = new CreditsBin();
+                    creditsBin.Field00 = br.ReadUInt32();
+                    creditsBin.RelocationTableOffset = br.ReadUInt32();
+                    creditsBin.RelocationTableSize = br.ReadUInt32();
+                    creditsBin.StringCount = br.ReadUInt32();
+                    creditsBin.StringLengthTableOffset = br.ReadUInt32();
+                    creditsBin.StringBufferOffset = br.ReadUInt32();
+                    creditsBin.StringBufferSize = br.ReadUInt32();
+                    fs.Seek(creditsBin.StringLengthTableOffset, SeekOrigin.Begin);
+                    for (int i = 0; i < creditsBin.StringCount; i++)
+                    {
+                        creditsBin.StringLengths.Add(br.ReadByte());
+                    }
+                    fs.Seek(creditsBin.StringBufferOffset, SeekOrigin.Begin);
+                    for (int i = 0; i < creditsBin.StringCount; i++)
+                    {
+                        byte[] stringBytes = br.ReadBytes(creditsBin.StringLengths[i]);
+                        string str = System.Text.Encoding.UTF8.GetString(stringBytes);
+                        creditsBin.Strings.Add(str);
+                    }
+                    fs.Seek(creditsBin.RelocationTableOffset, SeekOrigin.Begin);
+                    for (int i = 0; i < creditsBin.RelocationTableSize; i++)
+                    {
+                        creditsBin.RelocationTable.Add(br.ReadByte());
+                    }
+                    File.WriteAllLines(outputTxtPath, creditsBin.Strings);
+                }
+            }
+            else
+            {
+                string inputTxtPath = args[0];
+                string outputBinPath = ".\\out.bin";
+                CreditsBin creditsBin = new CreditsBin();
+                string[] lines = File.ReadAllLines(inputTxtPath);
+                creditsBin.StringCount = (uint)lines.Length;
+                foreach (string line in lines)
+                {
+                    byte[] stringBytes = System.Text.Encoding.UTF8.GetBytes(line);
+                    if (stringBytes.TakeLast(2).ToArray() != new byte[] { 0x0D, 0x0A })
+                    {
+                        stringBytes = stringBytes.Concat(new byte[] { 0x09, 0x00, 0x0D, 0x0A }).ToArray();
+                    }
+
+                    byte length = (byte)stringBytes.Length;
+                    creditsBin.StringLengths.Add(length);
+                    creditsBin.Strings.Add(line);
+                }
+                using (FileStream fs = new FileStream(outputBinPath, FileMode.Create))
+                using (BinaryWriter bw = new BinaryWriter(fs))
+                {
+                    bw.Write(creditsBin.Field00);
+                    bw.Write(creditsBin.RelocationTableOffset);
+                    bw.Write(creditsBin.RelocationTableSize);
+                    bw.Write(creditsBin.StringCount);
+                    bw.Write(creditsBin.StringLengthTableOffset);
+                    bw.Write(creditsBin.StringBufferOffset);
+                    bw.Write(creditsBin.StringBufferSize);
+                    foreach (byte length in creditsBin.StringLengths)
+                    {
+                        bw.Write(length);
+                    }
+                    foreach (string str in creditsBin.Strings)
+                    {
+                        byte[] stringBytes = System.Text.Encoding.UTF8.GetBytes(str);
+                        if (stringBytes.TakeLast(2).ToArray() != new byte[] { 0x0D, 0x0A })
+                        {
+                            stringBytes = stringBytes.Concat(new byte[] { 0x09, 0x00, 0x0D, 0x0A }).ToArray();
+                        }
+                        bw.Write(stringBytes);
+                    }
+                    foreach (byte b in creditsBin.RelocationTable)
+                    {
+                        bw.Write(b);
+                    }
+                }
+            }
+            
+
+        }
+
+        public class CreditsBin
+        {
+            public uint Field00 { get; set; } = 1527;
+            public uint RelocationTableOffset { get; set; } = 27312;
+            public uint RelocationTableSize { get; set; } = 1578;
+            public uint StringCount { get; set; } = 1320;
+            public uint StringLengthTableOffset { get; set; } = 28;
+            public uint StringBufferOffset { get; set; } = 1348;
+            public uint StringBufferSize { get; set; } = 25964;
+            public List<Byte> StringLengths { get; set; } = new List<Byte>();
+            public List<string> Strings { get; set; } = new List<string>();
+            public List<Byte> RelocationTable { get; set; } = new List<Byte>();
+
+        }
+    }
+}
